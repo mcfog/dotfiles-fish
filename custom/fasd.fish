d07029652d29b5cc54d90b268aa5795d7d94a811
McFog Wang <mcfogw@gmail.com>
2015-12-24 12:01:25 +0800

    better j

--- custom/fasd.fish
+++ custom/fasd.fish
@@ -3,15 +3,6 @@ function -e fish_preexec _run_fasd
      fasd --proc (fasd --sanitize $argv | tr -s ' ' \n) > /dev/null 2>&1
 end
 
-function j
-    set -l dir (fasd -de "printf %s" "$argv")
-    if test "$dir" = ""
-        echo "no matching directory"
-        return 1
-    end
-    cd $dir
-end
-
 function f
     fasd -f "$argv"
 end
---
+++ functions/j.fish
@@ -0,0 +1,8 @@
+function j
+	fasd -Rdl "$argv[1]" | fzf -1 -0 --no-sort +m | read -l dir
+  if [ $dir ]
+    cd $dir
+  else
+    return 1
+  end
+end
