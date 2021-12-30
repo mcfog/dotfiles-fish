adb4e54fac54a13753bdb1fc6619f63dd7764d37
McFog W <mcfogw@gmail.com>
2021-12-30 14:03:25 +0800

    ✨ update

--- functions/proxy.fish
+++ functions/proxy.fish
@@ -1,16 +1,18 @@
 function proxy -d "print or set HTTP proxy"
 	if set -q argv[1]
-		set -l last $HTTP_PROXY
+		set -l last $ALL_PROXY
 
 		if [ $argv[1] = "direct" ]
 			set -e HTTP_PROXY
 			set -e HTTPS_PROXY
+			set -e ALL_PROXY
 
 			echo -n "cleared, "
 		else
 			set -l last $HTTP_PROXY
 			set -gx HTTP_PROXY $argv[1]
 			set -gx HTTPS_PROXY $argv[1]
+			set -gx ALL_PROXY $argv[1]
 
 			echo -n "ok. "
 		end
@@ -21,6 +23,6 @@ function proxy -d "print or set HTTP proxy"
 			echo "was empty"
 		end
 	else
-		echo $HTTP_PROXY
+		echo $ALL_PROXY
 	end
 end
